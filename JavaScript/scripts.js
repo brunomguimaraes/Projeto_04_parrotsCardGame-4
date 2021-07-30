3cfcd2e287e7963c94b9e9bf5ecfbbdf78c05a49
Carlos <carlosedufpaiva@poli.ufrj.br>
2021-07-30 15:11:38 -0300

    Implemented Features: 1 - It was created a scoring system, and the score for each game is included to the final message. / 2 - At the beginning of each game, it is asked the name of the user, which is also added to the final message. / 3 - At the right side of the desktop and at the bottom of the mobile version it is now included a ranking that gets updated at the end of each game. The ranking is ordered by the scores of all players.

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -17,12 +17,14 @@ let FirstActivedImage;
 let RoundsCounter = 0;
 let SecondsCounter = 0;
 let TimerID;
+let Ranking = document.querySelector(".ranking-list");
+let AllPlayers= [];
+let ActivePlayer = ["",0] /* [Player Name, Player pontuation] */
 
 /* -------- End of the global variables -------- */
 
 /* -------- Beginning of the functions running at the opening of the page -------- */
 
-PickNumberOfCards ();
 StartTheGame ();
 
 /* -------- End of the functions running at the opening of the page -------- */
@@ -37,7 +39,7 @@ function PickNumberOfCards () {
 }
 
 function comparison() { 
-	return Math.random() - 0.5; 
+    return Math.random() - 0.5; 
 }
 
 function ShuffleCards() {
@@ -77,10 +79,16 @@ function StartTimer(Clock) {
 
 
 function StartTheGame () {
+    let ActivePlayerName = prompt("Qual é o seu nome?");
+    while (!ActivePlayerName) {
+        ActivePlayerName = prompt("Qual é o seu nome?");
+    }
+    ActivePlayer[0] = ActivePlayerName; 
+    PickNumberOfCards ();
     let ShuffledCards = ShuffleCards();
-    let CardsList = "";
+    let CardsHTML = "";
     for (let i = 0 ; i < NumberOfCards; i++) {
-        CardsList +=`<li onclick="ActivateCard(this)";>
+        CardsHTML +=`<li onclick="ActivateCard(this)";>
                         <div class="front-face">
                             <img src=${FrontCardImage}/>
                         </div>
@@ -89,14 +97,19 @@ function StartTheGame () {
                         </div>
                     </li>`;
     }
-    document.querySelector(".cards").innerHTML = CardsList;
+    document.querySelector(".cards").innerHTML = CardsHTML;
     CorrectCards = 0;
     RoundsCounter = 0;
     StartTimer(document.querySelector(".clock"));
 }
 
+function CalculateActiveResult() {
+    let ActivePlayerResult = (NumberOfCards*100) - (RoundsCounter*10) - (SecondsCounter);
+    ActivePlayer[1] = ActivePlayerResult
+}
+
 function WriteFinalMessage () {
-    let FinalMessage = `Parabéns! Você ganhou em ${RoundsCounter} Rodadas! Seu tempo foi de `;
+    let FinalMessage = `Parabéns, ${ActivePlayer[0]}! Você ganhou em ${RoundsCounter} rodadas e o seu tempo foi de `;
     if ((SecondsCounter - SecondsCounter%60) !== 0) {
         FinalMessage += `${(SecondsCounter - SecondsCounter%60)/60}`;
         if ((SecondsCounter - SecondsCounter%60)/60 === 1){
@@ -105,18 +118,43 @@ function WriteFinalMessage () {
             FinalMessage += ` minutos e `;
         }
     }
-    FinalMessage += `${SecondsCounter%60} segundos!`;
+    FinalMessage += `${SecondsCounter%60} segundos! Você fez incríveis ${ActivePlayer[1]} pontos!`;
     return FinalMessage;
+}
+
+function UpdateRanking(){
+    let PlayersString = ``;
+    let ResultsString = ``;
+    let Player = ActivePlayer.slice();
+    let InclusionOfPlayer = false;
+    if (AllPlayers.length === 0) {
+        AllPlayers.push(Player);        
+        PlayersString += `<li>1° - ${Player[0]}</li>`;
+        ResultsString += `<li><span>${Player[1]} Pontos</span></li>`;
+    } else {
+        for (let i = 0 ; i < AllPlayers.length ; i++){
+            if (Player[1] > AllPlayers[i][1] && !InclusionOfPlayer) {
+                AllPlayers.splice(i,0,Player);
+                InclusionOfPlayer = true;
+            }
+            PlayersString += `<li>${i+1}° - ${AllPlayers[i][0]}</li>`;
+            ResultsString += `<li><span>${AllPlayers[i][1]} Pontos</span></li>`;
+        }
+    }
+    Ranking.querySelector(".players").innerHTML = PlayersString;
+    Ranking.querySelector(".results").innerHTML = ResultsString;
 }
 
 function CheckEndOfGame() {
     if (CorrectCards === NumberOfCards ) {
         clearInterval(TimerID);
+        CalculateActiveResult();
+        UpdateRanking();
         FinalMessage = WriteFinalMessage();
         alert(FinalMessage);
         let CheckRestartGame = prompt("Gostaria de reiniciar o jogo?");
-        if (CheckRestartGame === "Sim" || CheckRestartGame === "sim" || CheckRestartGame === "SIM" || CheckRestartGame === "S"|| CheckRestartGame === "s") {
-            PickNumberOfCards ();
+        let AcceptableRestartAnswers = ["Sim","sim","SIM","S","s","Yes","yes","YES","Y","y"]
+        if (AcceptableRestartAnswers.includes(CheckRestartGame)) {
             StartTheGame ();
         }
     }
@@ -129,21 +167,20 @@ function FlipTwoWrongCards (ClickedLi,FirstActivedImage) {
 }
 
 function TestSecondCard(ClickedLi,ActiveCard) {
-    ClickedLi.classList.add("turned-up");
     if (ActiveCard.src === FirstActivedImage.src) { /*If player got a correct pair*/
         CorrectCards += 2;
     } else {  /*If player got a wrong pair*/
         setTimeout(FlipTwoWrongCards,1000,ClickedLi,FirstActivedImage);
     }
-    FirstActivedImage = undefined; /* Starting a new round*/
+    FirstActivedImage = false; /* Starting a new round*/
     RoundsCounter += 1;
     setTimeout(CheckEndOfGame,100);
 }
 
 function ActivateCard(ClickedLi){
     let ActiveImage = ClickedLi.querySelector(".back-face img");
+    ClickedLi.classList.add("turned-up");
     if (!FirstActivedImage){ /*If this is the first card played*/
-        ClickedLi.classList.add("turned-up");
         FirstActivedImage = ActiveImage;
     } else { /*If this is the second card played*/
         if (ActiveImage.id !== FirstActivedImage.id) { /*Making sure the clicked card is not the first one played*/
